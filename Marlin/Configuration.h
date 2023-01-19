29781b7b819d3614a1e9cd7cd7c56fcae4ef26d6
S. Erisman <github@serisman.com>
2023-01-18 23:27:30 -0600

    Calibrated steps for E0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1171,8 +1171,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-// TODO... Configure E steps (X/Y/Z already confirmed)
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 140 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
